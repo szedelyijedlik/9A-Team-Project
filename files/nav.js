bbd997ddb3672b16f2ec1b7b4d16f4a6dcea5d79
Szédelyi Ábris Péter <szedelyi.abris.peter@students.jedlik.eu>
2022-11-30 08:24:29 +0100

    v0.00 HTML oldalak

---
+++ files/nav.js
@@ -0,0 +1,9 @@
+function openMenu(){
+    var links = document.querySelector(".links");
+    links.style.right = 0;
+}
+
+function closeMenu(){
+    var links = document.querySelector(".links");
+    links.style.right = "-100%";
+}
